2db1de5ae13582aa51d43bdf2f3967c1c082d7d4
yash18102004 <155449726+yash18102004@users.noreply.github.com>
2024-02-02 17:06:08 +0000

    conversion of datatypes

---
+++ basics/operationconverstion.js
@@ -0,0 +1,10 @@
+let score = true;
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=33
+//"33abc"=NaN
+//true=1 false=0
