c16da45d946b41c4212c2666dd6aa3af15caa91b
yash18102004 <155449726+yash18102004@users.noreply.github.com>
2024-02-02 17:41:56 +0000

    datatypes summary

---
+++ basics/daattypes-summury.js
@@ -0,0 +1,21 @@
+// Primitive
+
+//7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt 
+
+//Reference (Non primitives): Array, Object, Function
+
+//Array :
+const heros = ['shaktiman','thor','ironman']
+
+
+//object:
+let myObj = {
+    name: "yash",
+    age: 19,
+}
+
+//Function
+const myFunction = function(){
+    console.log("Hello world")
+}
+console.log(typeof myFunction)
