deb40c1f0ebb5c932f2ede74433836b9d9533e6f
yash18102004 <155449726+yash18102004@users.noreply.github.com>
2024-02-05 12:46:34 +0000

    math

---
+++ basics/math.js
@@ -0,0 +1,24 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.9))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(3,2,8,6,9))
+// console.log(Math.max(3,2,8,6,9))
+
+
+console.log(Math.random())
+console.log(Math.random()*10)
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + max)
+
+
+
+
+
+
